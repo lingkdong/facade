53ab39092b85d859090590ea867245de5631e10b
dongxin <2524964431@qq.com>
2021-05-13 11:07:30 +0800

    init

---
+++ src/test/java/com/prefox/facade/FacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prefox.facade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
